4ab6df5c8d29b8d491aabdcf525be484a7121505
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-10-16 10:13:18 +0200

    création des fichiers

---
+++ assets/js/modules/catalogue.js
@@ -0,0 +1,12 @@
+const catalogue = [
+    {
+        tire : "blabla",
+        artiste : "blabla",
+        genre : "blabla",
+        annee : 1794,
+        cover : "blabla.jpg",
+        audio : "blabla.mp3",
+        album : "blabla",
+        like : false,
+    }
+]
