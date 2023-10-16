8700a352d35974c7cb8e6af596b725dada7c1a38
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-10-16 16:57:21 +0200

    modif audio.js

--- assets/js/modules/audio.js
+++ assets/js/modules/audio.js
@@ -2,6 +2,6 @@ const audio = (catalogue, currentTrack) => {
   console.log("initialisation piste audio");
   const urlAudio = "./assets/audio/";
   let track = new Audio(urlAudio + catalogue[currentTrack].audio);
-  console.log(track);
+  console.dir(track);
 };
 export { audio };
