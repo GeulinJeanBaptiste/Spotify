447dff146ed189372cfc4921b5cb372f94811bc4
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-10-17 16:02:22 +0200

    slider.css

--- assets/js/modules/slider.js
+++ assets/js/modules/slider.js
@@ -1,19 +1,23 @@
+const sliderHTML = document.querySelector("#slider");
+const coverUrl = "./assets/img/covers/";
+
+const initSlider = () => {
+  // Je cree une première image d'arrière plan fixe.
+  const coversSlider = document.createElement("img");
+  coversSlider.src = coverUrl + catalogue[currentTrack].cover;
+  coversSlider.id = "coversSlider";
+  sliderHTML.append(coversSlider);
+  // Je crée une deuxieme image supperposée destinée à l'effet (transform).
+  const imgA = document.createElement("img");
+  imgA.src = coverUrl + catalogue[currentTrack].cover;
+  imgA.id = "imgA";
+  sliderHTML.append(imgA);
+};
 const slider = (status = "init") => {
   console.log("initialisation du slider");
-  //   console.dir(catalogue);
-  //   console.log(catalogue[0].cover);
-  const sliderHTML = document.querySelector("#slider");
-  const coverUrl = "./assets/img/covers/";
-
   switch (status) {
     case "init":
-      const coversSlider = document.createElement("img");
-      coversSlider.src = coverUrl + catalogue[currentTrack].cover;
-      coversSlider.id = "coversSlider";
-      //   prepend insert un element après ceux qui existent déjà dans le parent.
-      //   sliderHTML.prepend(coversSlider);
-      //   append insert un element après ceux qui existent déjà dans le parent.
-      sliderHTML.append(coversSlider);
+      initSlider();
       break;
     case "next":
       document.querySelector("#coversSlider").src =
